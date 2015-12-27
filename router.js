d03a1ad93ff6cf1042194c20f83c98dde0db7460
Zach Latta <zach@zachlatta.com>
2015-12-26 16:28:22 -0800

    Parse incoming attachments

--- router.js
+++ router.js
@@ -1,9 +1,21 @@
 import koaRouter from 'koa-router';
+import koaBody from 'koa-body';
 
+let bodyParser = koaBody({multipart: true});
 let router = koaRouter();
 
-router.get('/', function *() {
-  this.body = 'Hello World';
+router.post('/receive', bodyParser, function *() {
+  let body = this.request.body;
+
+  if (!body.hasOwnProperty('files')) {
+    console.error("No attachment found.");
+    this.response.body = "Error, no attachment found.";
+    this.response.status = 400;
+    return;
+  }
+
+  let attachments = body.files;
+  console.log(attachments);
 });
 
 export default router;
